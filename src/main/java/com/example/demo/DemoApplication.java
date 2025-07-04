3d33e807972ae017ab94bc272e51d686701e88c8
daegon7 <kdg8302@gmail.com>
2025-07-04 16:36:26 +0900

    add readme

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("############# cccccccccccccccccc"); SpringApplication.run(DemoApplication.class, args);
+		System.out.println("############# ddddddddddddddddd"); SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
